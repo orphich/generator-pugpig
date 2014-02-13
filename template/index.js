defef49a95acac99c95d4b204fb2b0d4455ed1ba
ed <eevans49h@gmail.com>
2014-02-13 16:01:42 +0000

    Updated page files updating when new templates are created

--- template/index.js
+++ template/index.js
@@ -3,6 +3,12 @@ var util = require('util');
 var yeoman = require('yeoman-generator');
 var fs = require('fs');
 
+var pagesPath = process.cwd() + '/page-vars.json';
+var Pages = require(pagesPath);
+
+// var editionsPath = process.cwd() + '/edition-vars.json';
+// var Editions = require(editionPath);
+
 var TemplateGenerator = module.exports = function TemplateGenerator(args, options, config) {
   // By calling `NamedBase` here, we get the argument to the subgenerator call
   // as `this.name`.
@@ -53,15 +59,60 @@ TemplateGenerator.prototype.createSass = function create() {
 
 };
 
+TemplateGenerator.prototype.updatePages = function updatePages() {
+
+  var pages = Pages,
+    templateObj;
+
+  templateObj = {
+    fsName: this.fsTemplateName,
+    name: this.name,
+    description: this.description
+  };
+
+  pages.push(templateObj);
+
+  fs.writeFile(pagesPath, JSON.stringify(pages, null, 2), function(err) {
+    if(err) {
+      console.log(err);
+    } else {
+      console.log('Pages object saved to JSON file');
+    }
+  });
+
+  console.log('Page Object updated');
+
+};
+
+TemplateGenerator.prototype.updateEditions = function updateEditions() {
+
+  // var editions = Editions,
+  //   outputFilename = jsonPath,
+  //   templateObj;
+
+  // templateObj = {
+  //   fsName: this.fsTemplateName,
+  //   name: this.name,
+  //   description: this.description
+  // };
+
+  // pages.push(templateObj);
+
+  // fs.writeFile(jsonPath, JSON.stringify(pages, null, 2), function(err) {
+  //   if(err) {
+  //     console.log(err);
+  //   } else {
+  //     console.log('Pages object saved to JSON file');
+  //   }
+  // });
+
+  // console.log('Page Object updated');
+
+};
+
 TemplateGenerator.prototype.updateTOC = function updateTOC() {
 
   this.appendToFile('index.html', 'ul', '  <li><a href="' + this.fsTemplateName + '.html">' + this.name + '</a></li>\n  ');
   console.log('Table of Contents updated');
-
-  // fs.appendFile('page-vars.php', '$pages["' + this.name + '"] = [\n  "short-name" => "' + this.fsTemplateName + '",\n  "manifest" => "' + this.fsTemplateName + '.manifest",\n  "description" => "' + this.description + '",\n];\n\n');
-  // console.log('Atom feed updated');
-
-  this.appendToFile('page-vars.js', 'ul', this.name +  '\n  this.pageArray.push({\n    fsName: \'' + this.fsTemplateName + '\',\n    name: \'' + this.name + '\',\n    description: \'' + this.description + '\'\n  });\n\n    // ');
-  console.log('Page Object updated');
 
 };
