9ecc2a65442b2200c551672a89a53662408f3f30
ed <eevans49h@gmail.com>
2014-02-13 15:59:40 +0000

    Updated atom and opds writing logic

--- app/templates/atom.js
+++ app/templates/atom.js
@@ -1,41 +1,40 @@
 'use strict';
 
-var Pages = require('./app/static/page-vars.js'),
-  Editions = require('./app/static/edition-vars.js'),
-  Hogan = require ('hogan.js'),
+var Hogan = require ('hogan.js'),
   moment = require('moment'),
   xmlStringTemp = '';
 
+var Pages = require('./app/static/page-vars.json');
+var Editions = require('./app/static/edition-vars.json');
+
 var feed = function(){
 
-  var pageObj = new Pages();
+  var pages = Pages;
+  var editions = Editions;
+  var dateString = editions.updatedDate;
+
 
   var constructAtomEntry = function(element, index, array) {
 
-    var entry, template, edition, editionDate, dateString;
-    edition = new Editions();
-    editionDate = moment(edition.buildDate);
-    dateString = editionDate.format();
+    var entry, template, dateString;
 
     var data = {
       name: element.name,
       fsName: element.fsName,
       desc: element.description,
       section: 'News',
-      date: dateString,
+      date: dateString
     };
 
-    template = Hogan.compile('  <entry>\n  <title>{{name}}</title>\n    <id>{{fsName}}</id>\n    <updated>2013-09-12T00:12:00+00:00</updated>\n    <published>2013-09-12T00:12:00+00:00</published>\n    <summary>{{desc}}</summary>\n    <category scheme="http://schema.pugpig.com/section" term="{{section}}"/>\n    <link rel="alternate" type="text/html" href="{{fsName}}.html"/>\n    <link rel="related" type="text/cache-manifest" href="{{fsName}}.manifest"/>\n  </entry>\n\n');
+    template = Hogan.compile('  <entry>\n    <title>{{name}}</title>\n    <id>{{fsName}}</id>\n    <updated>{{date}}</updated>\n    <published>2013-09-12T00:12:00+00:00</published>\n    <summary>{{desc}}</summary>\n    <category scheme="http://schema.pugpig.com/section" term="{{section}}"/>\n    <link rel="alternate" type="text/html" href="{{fsName}}.html"/>\n    <link rel="related" type="text/cache-manifest" href="{{fsName}}.manifest"/>\n  </entry>\n\n');
 
     entry = template.render(data);
 
-    xmlStringTemp = xmlStringTemp + entry;
-
   };
 
-  pageObj.pageArray.forEach(constructAtomEntry);
+  pages.forEach(constructAtomEntry);
 
-  this.xmlStr = '<feed xmlns="http://www.w3.org/2005/Atom" xmlns:app="http://www.w3.org/2007/app">\n\n  <id>1234</id>\n  <title>Edition 1234</title>\n  <updated>' + dateString + '</updated>\n\n  <author>\n    <name>Kaldor Group</name>\n  </author>\n' + xmlStringTemp + '</feed>';
+  this.xmlStr = '<feed xmlns="http://www.w3.org/2005/Atom" xmlns:app="http://www.w3.org/2007/app">\n\n  <id>' + editions.editionArray[0].id + '</id>\n  <title>' + editions.editionArray[0].title + '</title>\n  <updated>' + dateString + '</updated>\n\n  <author>\n    <name>Kaldor Group</name>\n  </author>\n' + xmlStringTemp + '</feed>';
 
   // this.xmlStr = this.xmlstr + xmlStringTemp;
 
--- app/templates/opds.js
+++ app/templates/opds.js
@@ -1,14 +1,16 @@
 'use strict';
 
-var editions = require('./<%= yeoman.app %>/static/edition-vars.js');
 var moment = require('moment');
 var Hogan = require ('hogan.js');
 var xmlStringTemp = '';
 var updateDate;
 
+var Pages = require('./app/static/page-vars.json');
+var Editions = require('./app/static/edition-vars.json');
+
 var feed = function(){
 
-  var editionObj = new editions();
+  var editions = Editions;
 
   var constructOPDSEntry = function(element, index, array) {
 
@@ -22,19 +24,17 @@ var feed = function(){
       atomAlt: element.atomURLAlt
     };
 
-    var template = Hogan.compile('  <entry>\n  <title>{{name}}</title>\n    <id>{{fsName}}</id>\n    <updated>{{date}}</updated>\n    <dcterms:issued>2013-09-13</dcterms:issued>\n    <author>\n      <name>Pugpig</name>\n    </author>\n    <summary>{{desc}}</summary>\n    <link rel="http://opds-spec.org/image" type="image/jpg" href="{{cover}}"/>\n    <link rel="http://opds-spec.org/acquisition" type="application/atom+xml" href="{{atom}}"/>\n    <link rel="alternate" type="application/atom+xml" href="{{atomAlt}}"/>\n  </entry>\n\n');
+    var template = Hogan.compile('  <entry>\n    <title>{{name}}</title>\n    <id>{{fsName}}</id>\n    <updated>{{date}}</updated>\n    <dcterms:issued>2013-09-13</dcterms:issued>\n    <author>\n      <name>Pugpig</name>\n    </author>\n    <summary>{{desc}}</summary>\n    <link rel="http://opds-spec.org/image" type="image/jpg" href="{{cover}}"/>\n    <link rel="http://opds-spec.org/acquisition" type="application/atom+xml" href="{{atom}}"/>\n    <link rel="alternate" type="application/atom+xml" href="{{atomAlt}}"/>\n  </entry>\n\n');
 
     var output = template.render(data);
 
-    xmlStringTemp = xmlStringTemp + output;
-
   };
 
-  editionObj.editionArray.forEach(constructOPDSEntry);
+  editions.editionArray.forEach(constructOPDSEntry);
 
-  updateDate = moment(editionObj.buildDate).format();
+  updateDate = moment(editions.updatedDate).format();
 
-  this.xmlStr = '<?xml version="1.0" encoding="UTF-8"?>\n<feed xmlns="http://www.w3.org/2005/Atom" xmlns:dcterms="http://purl.org/dc/terms/" xmlns:opds="http://opds-spec.org/2010/catalog">\n\n  <id>1234</id>\n  <title>Edition 1234</title>\n  <updated>' + updateDate + '</updated>\n  <author>\n    <name>Pugpig</name>\n  </author>\n\n' + xmlStringTemp + '</feed>';
+  this.xmlStr = '<?xml version="1.0" encoding="UTF-8"?>\n<feed xmlns="http://www.w3.org/2005/Atom" xmlns:dcterms="http://purl.org/dc/terms/" xmlns:opds="http://opds-spec.org/2010/catalog">\n\n  <id>mag-id</id>\n  <title>Magazine title</title>\n  <updated>' + updateDate + '</updated>\n  <author>\n    <name>Pugpig</name>\n  </author>\n\n' + xmlStringTemp + '</feed>';
 
   // this.xmlStr = this.xmlstr + xmlStringTemp;
 
